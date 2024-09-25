a8a6d66694a9ac2dccabbbdfb33ae2000b7e90de
Sweta-J <73520213+Sweta-J@users.noreply.github.com>
2024-09-25 16:50:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function toHall() {    
+    window.location = "hall.html";
+}
+
+function toDesk() {    
+    window.location = "desk.html";
+}
+
+function toBottles() {    
+    window.location = "bottle.html";
+}
+
+function toFruit() {    
+    window.location = "fruit.html";
+}
+
+function toBedroom() {    
+    window.location = "bedroom.html";
+}
